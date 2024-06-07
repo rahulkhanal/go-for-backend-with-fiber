4d4749d840c9c34f14bed80396b87f8fc13655f8
Rahul Khanal <92104751+rahulkhanal@users.noreply.github.com>
2024-06-07 22:19:30 +0545

    mysql database connection

--- app.go
+++ app.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"godoc/src/routes"
+	db "godoc/src/config"
+	route "godoc/src/routes"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 )
+
+var DB *gorm.DB
 
 func main() {
 	app := fiber.New()
-	godotenv.Load(".env")      // load env
-	routes.RegisterRoutes(app) //routing
-	println("khai" + os.Getenv("PORT"))
+	db.Connect()
+	godotenv.Load(".env") // load env
+	// database
+	route.RegisterRoutes(app) //routing
 	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 }
---
+++ src/config/sql.config.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"fmt"
+	models "godoc/src/model/sql"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	godotenv.Load()
+	DbHost := os.Getenv("MYSQL_HOST")
+	DbName := os.Getenv("MYSQL_DBNAME")
+	DbUsername := os.Getenv("MYSQL_USER")
+	DbPassword := os.Getenv("MYSQL_PASSWORD")
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", DbUsername, DbPassword, DbHost, DbName)
+	dbConnection, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+
+	if err != nil {
+		panic("connection failed to the database ")
+	}
+	DB = dbConnection
+	fmt.Println("db conncted successfully")
+
+	AutoMigrate(dbConnection)
+}
+
+func AutoMigrate(connection *gorm.DB) {
+	connection.Debug().AutoMigrate(&models.User{})
+}
